c2125fb1a11360f1ac7d3a36088121ec8beb19e2
selly361 <104414846+selly361@users.noreply.github.com>
2023-03-09 13:15:14 +0000

    Change: setted up cors middleware

--- server.js
+++ server.js
@@ -1,15 +1,24 @@
+require("module-alias/register");
+require("dotenv").config();
 const express = require("express");
-require("dotenv").config()
-require('module-alias/register')
+const cors = require("cors");
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-
+app.use(
+  cors({
+    origin:
+      process.env.NODE_ENV === "development"
+        ? true
+        : process.env.NODE_ENV === "production"
+        ? process.env.ORIGIN
+        : false
+  })
+);
 
 
 const PORT = process.env.PORT || 3001;
 
-
-app.listen(PORT, () => console.log(`Listening on port ${PORT}...`))
+app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
