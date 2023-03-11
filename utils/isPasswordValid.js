048ce282af26f825e657a9e490c6807586eb07dc
seikou <seikou615@gmail.com>
2023-03-11 09:22:08 +0000

    Added: isPasswordValid function with JSDOCS for explanation

---
+++ utils/isPasswordValid.js
@@ -0,0 +1,23 @@
+/**
+ * Checks if a password is valid.
+ *
+ * @param {string} password - The password to validate.
+ * @returns {boolean} True if the password is valid, false otherwise.
+ */
+function isPasswordValid(password) {
+
+
+
+    /**
+     *  Regular expression that matches a valid username pattern.
+     * 
+     * @type {RegExp}
+     * 
+     */
+    
+    const passwordRegex = /^[a-zA-Z0-9]{5,20}$/;
+    
+    return passwordRegex.test(password);
+
+  }
+  
