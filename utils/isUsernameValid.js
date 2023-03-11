c51c6145700bf5d214cd3efce9301e14a3610051
seikou <seikou615@gmail.com>
2023-03-11 09:09:26 +0000

    Change: used jsdocs to make the function easier to understand

--- utils/isUsernameValid.js
+++ utils/isUsernameValid.js
@@ -1,8 +1,20 @@
-const isUsernameValid = (username) => /^[a-zA-Z0-9_-]+$/.test(username)
-
+/**
+ * Checks whether a given username string is valid.
+ *
+ * @param {string} username - The username to validate.
+ * @returns {boolean} Returns true if the username is valid, false otherwise.
+ *
+ */
+const isUsernameValid = (username) => {
+    /**
+     * Regular expression that matches a valid username pattern.
+     *
+     * @type {RegExp}
+     */
+    const usernamePattern = /^[a-zA-Z0-9_-]+$/;
   
-
-
-
+    return usernamePattern.test(username);
+  };
+  
   
 module.exports = isUsernameValid;
