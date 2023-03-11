bc8ba160fab7bf3e4be19177b3b4efc5d02088e6
seikou <seikou615@gmail.com>
2023-03-11 09:15:29 +0000

    Added: JSDOC to explain the function

--- utils/isEmailValid.js
+++ utils/isEmailValid.js
@@ -1,4 +1,23 @@
-const isEmailValid = (email) => /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email);
+/**
+ * Checks whether a given email string is valid
+ * 
+ * @param {string} email - The email to validate.
+ * 
+ * @returns {boolean} Returns true if the email is valid, false otherwise.
+ */
+
+
+
+const isEmailValid = (email) => {
+    
+    /**
+     * Regular expression that matches a valid email pattern.
+     *
+     * @type {RegExp}
+    */
+    
+    return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
+}
 
 
 
