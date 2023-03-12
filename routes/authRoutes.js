4c61dcff469c7b7866a30ae0174ec7a9a24e5de3
seikou <seikou615@gmail.com>
2023-03-12 08:50:49 +0000

    Added: route 'refresh-token'

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const validateInfo = require("@middlewares/validateInfo.js");
-const { registerController, loginController } = require("@authControllers")
+const { registerController, loginController, refreshTokenController } = require("@authControllers")
 
 router.post("/register", validateInfo,  registerController)
 
@@ -8,5 +8,6 @@ router.post("/register", validateInfo,  registerController)
 router.post("/login", validateInfo, loginController);
 
 
+router.get("/refresh-token", refreshTokenController);
 
 module.exports = router;
