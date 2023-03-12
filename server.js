025a9cc9b9e8d98061182b3f258ab31f3c7621d4
seikou <seikou615@gmail.com>
2023-03-12 08:31:09 +0000

    Changed: imported cookie parser, and use it as a middleware

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ require("module-alias/register");
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-
+const cookieParser = require("cookie-parser")
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser())
 
 app.use(
   cors({
