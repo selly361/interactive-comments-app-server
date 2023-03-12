0b7d5547d410c77015ef7da879b0744c993138d9
seikou <seikou615@gmail.com>
2023-03-12 06:18:25 +0000

    Added: authorize middeware, to verify jwt tokens

---
+++ middlewares/authorize.js
@@ -0,0 +1,26 @@
+const jwt = require("jsonwebtoken");
+
+module.exports =  (req, res, next) => {
+  const token = req.header("jwt_token");
+
+  if (!token) {
+    const error = {
+      error: "Authorization denied",
+      code: "AUTHORIZATION_DENIED",
+    };
+    return res.status(403).json(error);
+  }
+
+  try {
+    const verify = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    const { user_id } = verify;
+    req.user_id = user_id;
+    next();
+  } catch (err) {
+    const error = {
+      error: "Token is not valid",
+      code: "INVALID_TOKEN",
+    };
+    return res.status(401).json(error);
+  }
+};
