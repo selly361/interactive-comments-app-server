60b8194fc1e8eb38d96020b622712ac962775075
selly361 <104414846+selly361@users.noreply.github.com>
2023-03-22 12:18:04 +0000

    Removed: unnecessary module

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,5 @@
 const router = require("express").Router()
 const authorize = require("@authorizeMW")
-const db = require("@db");
 const { userDataController } = require("");
 
 router.get("/data", authorize, userDataController)
