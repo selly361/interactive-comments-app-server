3156c4d3a05a633fb477951cb3d267ebe62576c6
seikou <seikou615@gmail.com>
2023-03-22 06:20:57 +0000

    Added: `userDataController`

---
+++ controllers/userControllers.js
@@ -0,0 +1,16 @@
+const db = require("@db");
+
+const userDataController = async (req, res) => {
+  let query = `
+        SELECT username, profile_image FROM users
+        WHERE user_id = $1
+        `;
+
+  const { rows } = await db.query(query, [req.user_id]);
+
+  res.json({ ...rows[0] });
+};
+
+
+
+module.exports = { userDataController }
