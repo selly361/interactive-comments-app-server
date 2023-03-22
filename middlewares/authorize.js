2cb09fab158ac4edda8ca4f7dfa1717757f0efb0
seikou <seikou615@gmail.com>
2023-03-22 06:21:30 +0000

    Fixed: `authorize` middleware

--- middlewares/authorize.js
+++ middlewares/authorize.js
@@ -1,9 +1,9 @@
 const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
-  const authHeader = req.headers.authorization.split(" ");
+  const authHeader = req.headers?.authorization;
 
-  if (!authHeader.length || !authHeader[0] == "Bearer ") {
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
     const error = {
       error: "Authorization denied",
       code: "AUTHORIZATION_DENIED",
@@ -11,14 +11,15 @@ module.exports = (req, res, next) => {
     return res.status(403).json(error);
   }
 
-  const token = authHeader[1];
-
+  const token = authHeader.substring(7);
+  
   try {
     const verify = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
     const { user_id } = verify;
     req.user_id = user_id;
     next();
   } catch (err) {
+    console.error(err);
     const error = {
       error: "Token is not valid",
       code: "INVALID_TOKEN",
