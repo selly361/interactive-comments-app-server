84715ece438f77c161fe452aa38552a61e0e224d
seikou <seikou615@gmail.com>
2023-03-22 06:22:06 +0000

    used the user data controller

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,19 +1,9 @@
 const router = require("express").Router()
 const authorize = require("@authorizeMW")
-const db = require("@db")
+const db = require("@db");
+const { userDataController } = require("");
 
-router.get("/user", authorize, (req, res) => {
-    const userId = req;
-
-    let query = `
-    SELECT * FROM users
-    WHERE user_id = $1
-    `
-
-    const user = db.query(query, [userId])
-
-    console.log(user)
-})
+router.get("/data", authorize, userDataController)
 
 
 
