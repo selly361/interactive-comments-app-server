aa03272d5bff48ad75cae5b08cbfeb6c08780f7c
selly361 <104414846+selly361@users.noreply.github.com>
2023-03-22 12:58:58 +0000

    Added: a rate limiter for creating accounts

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,8 +1,17 @@
 const router = require("express").Router();
 const validateInfo = require("@validateMW");
 const controllers = require("@authControllers");
+const rateLimit = require("express-rate-limit");
 
-router.post("/register", validateInfo, controllers.registerController);
+const createAccountLimiter = rateLimit({
+	windowMs: 60 * 60 * 1000,
+	max: 5, 
+	message: 'Too many accounts created from this IP, please try again after an hour',
+	standardHeaders: true,
+	legacyHeaders: false, 
+})
+
+router.post("/register", [validateInfo, createAccountLimiter], controllers.registerController);
 
 router.post("/login", validateInfo, controllers.loginController);
 
