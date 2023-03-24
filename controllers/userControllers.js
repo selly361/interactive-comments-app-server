9cdf8c994b4dfc9da2d8a8f84c58a35c07167a84
selly361 <104414846+selly361@users.noreply.github.com>
2023-03-24 12:38:41 +0000

    Update userControllers.js

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -3,7 +3,7 @@ const db = require("@db");
 const userDataController = async (req, res) => {
   let query = `
         SELECT username, profile_image FROM users
-        WHERE user_id = $1
+        WHERE user_id = $1;
         `;
 
   const { rows } = await db.query(query, [req.user_id]);
