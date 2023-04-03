4e0cff5c237541a8e36277d0f29b6b77760eaa19
seikou <seikou615@gmail.com>
2023-04-03 15:45:12 +0100

    Removed: `verifyAuth` route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -5,12 +5,11 @@ const limiters = require('@rateLimitConfigs')
 
 router.post('/register', [validateInfo, limiters.createAccountLimiter], controllers.registerController)
 
-router.post('/login', [limiters.loginLimiter], controllers.loginController)
+router.post('/login', [validateInfo, limiters.loginLimiter], controllers.loginController)
 
 router.post('/logout', controllers.logoutController)
 
 router.get('/refresh-token', [limiters.refreshTokenLimiter], controllers.refreshTokenController)
 
-router.post('/verify-auth', [limiters.verifyAuthLimiter], controllers.verifyAuth)
 
 module.exports = router
